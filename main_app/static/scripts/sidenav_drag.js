818dbadb77c41bc63ed1d1f9bf949345f3ba2a54
LouisPino <ljpino95@gmail.com>
2023-09-28 17:27:43 -0400

    sidenav stretch works

--- main_app/static/scripts/sidenav_drag.js
+++ main_app/static/scripts/sidenav_drag.js
@@ -11,12 +11,19 @@ document.addEventListener('mouseup', function() {
     isDown = false;
 }, true);
 document.addEventListener('mousemove', function(e) {
-    console.log(divOverlay.getBoundingClientRect().width)
     if (isDown) {
         divOverlay.style.left = (e.clientX + offset) + 'px'
         divOverlay.style.width = (document.body.clientWidth - Number(divOverlay.style.left.split('px')[0]))+'px'
     }  
-    if(divOverlay.getBoundingClientRect().width < 301 || divOverlay.getBoundingClientRect().width > 1000){
+    if(divOverlay.getBoundingClientRect().width < 299 || divOverlay.getBoundingClientRect().width > 1000){
       isDown=false
-} 
+      if(divOverlay.getBoundingClientRect().width > 900){
+          oldWidth = Number(divOverlay.style.width.split('px')[0])
+          divOverlay.style.width = '995px'
+          divOverlay.style.left =  Number(divOverlay.style.left.split('px')[0])-(995-oldWidth) + 'px'
+      }else{
+      oldWidth = Number(divOverlay.style.width.split('px')[0])
+      divOverlay.style.width = '305px'
+      divOverlay.style.left =  Number(divOverlay.style.left.split('px')[0])-(305-oldWidth) + 'px'
+} }
 }, true);
